92115ff6fc5647406f1fbfc0a64c859e2b350f77
umair-shafiq <116309001+umair-shafiq@users.noreply.github.com>
2023-03-21 13:45:02 +0500

    Initial commit

---
+++ CrudApplication/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using CrudApplication.Context;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+
+        StudentDBEntities dbObj = new StudentDBEntities();
+        public ActionResult Student(student obj)
+        {
+            if(obj != null)
+            {
+                return View(obj);
+            }
+            else {
+                return View();
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(student model)
+        {
+            student obj = new student();
+            if (ModelState.IsValid)
+            {
+                obj.ID = model.ID;
+                obj.Name = model.Name;
+                obj.Fname = model.Fname;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+
+                if (model.ID == 0)
+                {
+                    dbObj.student.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                else
+                {
+                    dbObj.Entry(obj).State = EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+
+            }
+
+            ModelState.Clear();
+            return View("Student");
+        }
+
+        public ActionResult StudentList()
+        {
+            var res = dbObj.student.ToList();
+            return View(res);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.student.Where(x => x.ID == id).First();
+            dbObj.student.Remove(res);
+            dbObj.SaveChanges();
+
+            var list = dbObj.student.ToList();
+            return View("StudentList", list);
+        }
+    }
+}
---
+++ CrudApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrudApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrudApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("78216d63-16ed-46b2-b68f-7936d38c4542")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
